c762ccd8aa33109f5fb1af5ff24c341881b1ac6d
Byron Salazar <adx09adx@gmail.com>
2025-11-22 17:27:22 -0500

    Add Script1.c to calculate the sum of two numbers

---
+++ Scripts de Prueba/Script1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int num1 = 50;
+    int num2 = 100;
+    int total;
+
+    total = num1 + num2;
+
+    printf("El resultado es: %d", total);
+
+    return 0;
+}
